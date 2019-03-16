1cb30617dd10ba812bc9c67a3bd9f5152fc8d849
Assela Pathirana <assela@pathirana.net>
2019-03-16 15:01:00 +0100

    First comment - already working

---
+++ app.py
@@ -0,0 +1,124 @@
+import base64
+import os
+from urllib.parse import quote as urlquote
+
+from flask import Flask, send_from_directory
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import tempfile
+import predict
+import numpy as np
+import cv2
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Normally, Dash creates its own Flask server internally. By creating our own,
+# we can create a route for downloading files directly:
+server = Flask(__name__)
+app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
+
+UPLOAD_DIRECTORY = "/project/app_uploaded_files"
+
+# load the CNN 
+
+graph, cnn_model, cnn_lb = predict.load_model_and_labels('concrete.model','concrete_lb.pickle')
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+@server.route("/download/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True,
+        accept="image/*",
+        max_size="2100000", # 2 MB
+        
+    ),
+    html.Div(id='image_ul'),
+])
+
+
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def data_uri_to_cv2_img(uri):
+    encoded_data = uri.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+
+def process_image(fn, content):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    data = data_uri_to_cv2_img(content) #endode if needed 
+    #decode if needed dc=base64.decodebytes(data)
+    t,ext=os.path.splitext(fn)    
+    #output = predict.predict(graph, cnn_model, cnn_lb, data, 64, 64)	
+    preds = predict.predict2(graph, cnn_model, data, 64, 64)
+    i = preds.argmax(axis=1)[0]
+    label = cnn_lb.classes_[i]
+    # draw the class label + probability on the output image
+    text = "{}: {:.2f}%".format(label, preds[0][i] * 100)    
+    return html.Div(text), html.Img(src=content)
+
+
+@app.callback(
+    Output("image_ul", "children"),
+              [Input('upload-data', 'filename'),
+               Input('upload-data', 'contents'),
+              Input('upload-data', 'last_modified')])
+def update_output(uploaded_filename, uploaded_file_contents, modified_date):
+    """Save uploaded files and regenerate the file list."""
+
+    if uploaded_filename is not None and uploaded_file_contents is not None:
+        if isinstance(uploaded_filename, str): # its a list of files
+            uploaded_filename = [uploaded_filename]
+            uploaded_file_contents = [uploaded_file_contents]
+            modified_date = [modified_date]
+        res=[]
+        for file, content in zip(uploaded_filename,uploaded_file_contents):
+            #fix this to process all files are return
+            # try https://dash.plot.ly/datatable 
+            res1, res2 = process_image(file, content)
+            res.append(html.Table([html.Tr([html.Td(res1)]),
+                               html.Tr([html.Td(res2)])])
+                       )
+        return html.Div(id="results", children=res)
+                
+
+
+if __name__ == "__main__":
+    import os
+    if 'WINGDB_ACTIVE' in os.environ:
+        server.debug = False
+        server.run(port=8888)
---
+++ predict.py
@@ -0,0 +1,63 @@
+# USAGE
+# python predict.py --image images/dog.jpg --model output/simple_nn.model --label-bin output/simple_nn_lb.pickle --width 32 --height 32 --flatten 1
+# python predict.py --image concrete/Negative/00014.jpg --model output/concrete.model --label-bin output/concrete_lb.pickle --width 64 --height 64
+
+# import the necessary packages
+from keras.models import load_model
+from tensorflow import get_default_graph
+from pyimagesearch.smallvggnet import dumb
+import pickle
+import cv2
+from urllib.request import urlopen
+import numpy as np
+
+
+def load_model_and_labels(model_name, label_bin):
+	# load the model and label binarizer
+	print("[INFOx] loading network and label binarizer...")
+	with open(label_bin, "rb") as f_:
+		lb = pickle.loads(f_.read())
+	model = load_model(model_name)
+	graph = get_default_graph()
+	
+
+	return graph, model, lb
+
+def predict(graph, model, lb, image,x_,y_):
+	output = image.copy()
+	preds = predict2(image, x_, y_, graph, model)
+	# find the class label index with the largest corresponding
+	# probability
+	i = preds.argmax(axis=1)[0]
+	label = lb.classes_[i]
+
+	# draw the class label + probability on the output image
+	text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
+	cv2.putText(output, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+		(0, 0, 255), 2)
+
+	return output
+
+def predict2(graph, model, image, x_, y_):
+	
+	image = cv2.resize(image, (x_,y_))
+	# scale the pixel values to [0, 1]
+	image = image.astype("float") / 255.0
+	# working with a CNN -- don't flatten the
+	# image, simply add the batch dimension
+	image = image.reshape((1, image.shape[0], image.shape[1],
+		image.shape[2]))
+	# make a prediction on the image
+	with graph.as_default():
+		preds = model.predict(image)
+	return preds
+
+
+if __name__ == "__main__":
+	graph, model, lb = load_model_and_labels('concrete.model','concrete_lb.pickle')
+	# load the input image and resize it to the target spatial dimensions
+	image = cv2.imread('800px-Yin_yang.svg.png')
+	output = predict(graph, model, lb, image, 64, 64)	
+	# show the output image
+	cv2.imshow("Image", output)
+	cv2.waitKey(0)	
