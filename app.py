8c0d969dc270fd6e73b95fe242d9423351725254
Assela Pathirana <assela@pathirana.net>
2019-03-17 16:04:33 +0100

    color borders

--- app.py
+++ app.py
@@ -14,23 +14,11 @@ import cv2
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# Normally, Dash creates its own Flask server internally. By creating our own,
-# we can create a route for downloading files directly:
 server = Flask(__name__)
 app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 
-#UPLOAD_DIRECTORY = "/project/app_uploaded_files"
-
 # load the CNN 
-
 graph, cnn_model, cnn_lb = predict.load_model_and_labels('concrete.model','concrete_lb.pickle')
-#if not os.path.exists(UPLOAD_DIRECTORY):
-#    os.makedirs(UPLOAD_DIRECTORY)
-
-#@server.route("/download/<path:path>")
-#def download(path):
-#    """Serve a file from the upload directory."""
-#    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
 
 app.layout = html.Div([
@@ -60,17 +48,6 @@ app.layout = html.Div([
 ])
 
 
-
-#def uploaded_files():
-    #"""List the files in the upload directory."""
-    #files = []
-    #for filename in os.listdir(UPLOAD_DIRECTORY):
-        #path = os.path.join(UPLOAD_DIRECTORY, filename)
-        #if os.path.isfile(path):
-            #files.append(filename)
-    #return files
-
-
 def data_uri_to_cv2_img(uri):
     encoded_data = uri.split(',')[1]
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
@@ -88,8 +65,13 @@ def process_image(fn, content):
     i = preds.argmax(axis=1)[0]
     label = cnn_lb.classes_[i]
     # draw the class label + probability on the output image
+    if i==0:
+        bd = '5px solid green'
+    else:
+        bd = '5px solid red'
+
     text = "{}: {:.2f}%".format(label, preds[0][i] * 100)    
-    return html.Div(text), html.Img(src=content)
+    return html.Div(children=[html.Div(text),html.Img(src=content, width='227')],style=dict(border=bd))
 
 
 @app.callback(
@@ -107,13 +89,9 @@ def update_output(uploaded_filename, uploaded_file_contents, modified_date):
             modified_date = [modified_date]
         res=[]
         for file, content in zip(uploaded_filename,uploaded_file_contents):
-            #fix this to process all files are return
-            # try https://dash.plot.ly/datatable 
-            res1, res2 = process_image(file, content)
-            res.append(html.Table([html.Tr([html.Td(res1)]),
-                               html.Tr([html.Td(res2)])])
-                       )
-        return html.Div(id="results", children=res)
+            r_ = process_image(file, content)
+            res.append(r_)
+        return html.Div(id="results", children=res, className="row")
                 
 
 
